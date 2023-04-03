8ff77ca09680a86b060b2c7d4126e617ee2ce726
Lucy <122321793+num1dev@users.noreply.github.com>
2023-04-03 17:28:30 +0900

    Practice 1 - JAVA Basic 3

---
+++ 2nd-Week/2023.03.08/ControlLab2.java
@@ -0,0 +1,17 @@
+package day03;
+
+public class ControlLab2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 1;
+		while (i <= 10) {
+			i++;
+			if ((i-1) % 3 != 0 && (i-1) % 4 != 0) {
+				System.out.println((i-1));
+			} else {
+				continue;
+			}
+		}
+	}
+}
