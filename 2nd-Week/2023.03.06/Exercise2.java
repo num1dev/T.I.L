6463a60a5693887184a88939a75f467aa5be04f9
Lucy <122321793+num1dev@users.noreply.github.com>
2023-04-03 15:56:26 +0900

    Practice 2 - JAVA Basic

---
+++ 2nd-Week/2023.03.06/Exercise2.java
@@ -0,0 +1,12 @@
+package day01;
+
+public class Exercise2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 35;
+		int y = 10;
+		System.out.printf("%d 를 %d 으로 나눈 결과 몫은 %d 이고 나머지는 %d 입니다.", x , y , x / y , x % y);
+	}
+
+}
