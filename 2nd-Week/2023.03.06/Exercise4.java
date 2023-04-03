c1fca655f5f1d414d05937a202dc7b12fb027d5d
Lucy <122321793+num1dev@users.noreply.github.com>
2023-04-03 15:57:17 +0900

    Practice 4 - JAVA Basic

---
+++ 2nd-Week/2023.03.06/Exercise4.java
@@ -0,0 +1,24 @@
+package day01;
+
+public class Exercise4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int number = 100;
+		int result;
+		
+		result = number + 10;
+		System.out.printf("덧셈 연산의 결과 : %d\n", result);
+		
+		result = number - 10;
+		System.out.printf("뺄셈 연산의 결과 : %d\n", result);
+		
+		result = number * 10;
+		System.out.printf("곱셈 연산의 결과 : %d\n", result);
+		
+		result = number / 10;
+		System.out.printf("나눗셈 연산의 결과 : %d\n", result);
+
+	}
+
+}
