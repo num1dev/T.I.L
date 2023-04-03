baa4ebce4bf1a0c05065d22590f545cafd809f90
Lucy <122321793+num1dev@users.noreply.github.com>
2023-04-03 15:56:57 +0900

    Practice 3 - JAVA Basic

---
+++ 2nd-Week/2023.03.06/Exercise3.java
@@ -0,0 +1,13 @@
+package day01;
+
+public class Exercise3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char name1 = 'H';
+		char name2 = 'S';
+		char name3 = 'J';
+		System.out.println( "" + name1 + name2 + name3 );
+	}
+
+}
