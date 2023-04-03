4c11bff664b8fe126d96e96c388a0b664a8da0f4
Lucy <122321793+num1dev@users.noreply.github.com>
2023-04-03 15:55:53 +0900

    Practice 1 - JAVA Basic

---
+++ 2nd-Week/2023.03.06/Exercise1.java
@@ -0,0 +1,14 @@
+package day01;
+
+public class Exercise1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 10;
+		int y = 25;
+		int z = 33;
+		System.out.println("합계 : " + (x + y + z));
+		System.out.println("평균 : " + (x + y + z)/3);
+	}
+
+}
