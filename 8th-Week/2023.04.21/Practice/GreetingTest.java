fea7c373a6961766d69a49d8c02e8d874c344f92
Lucy <122321793+num1dev@users.noreply.github.com>
2023-04-27 11:41:15 +0900

    Practice - Spring Framework Basic 03

--- 8th-Week/2023.04.21/Practice/GreetingTest.java
+++ 8th-Week/2023.04.21/Practice/GreetingTest.java
@@ -7,7 +7,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import java.time.LocalDateTime;
 
 
-public class GreetingTest {
+public class GreetingTest { 
     public static void main(String[] args) {
              ApplicationContext factory
                 = new ClassPathXmlApplicationContext("exam3.xml");
