39e212190e9059792b86cdfa6a904ae58b90741a
Lucy <122321793+num1dev@users.noreply.github.com>
2023-04-27 10:39:28 +0900

    Spring Framework Basic

---
+++ 8th-Week/2023.04.20/Practice 1/GreetingTest.java
@@ -0,0 +1,30 @@
+package exam1;
+
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.time.LocalDateTime;
+
+
+public class GreetingTest {
+    public static void main(String[] args) {
+             ApplicationContext factory
+                = new ClassPathXmlApplicationContext("exam1.xml");
+            LocalDateTime now = (LocalDateTime)factory.getBean("DateTime");
+            int hour = now.getHour();
+            if (6 <= hour && hour < 12) {
+                Greeting bean1 =(Greeting)factory.getBean("Greeting1");
+                bean1.greet();
+            } else if (12 <= hour && hour < 17) {
+                Greeting bean2 =(Greeting)factory.getBean("Greeting2");
+                bean2.greet();
+            } else if (17 <= hour && hour < 22) {
+                Greeting bean3 =(Greeting)factory.getBean("Greeting3");
+                bean3.greet();
+            } else {
+                Greeting bean4 =(Greeting)factory.getBean("Greeting4");
+                bean4.greet();
+            }
+        }
+    }
---
+++ 8th-Week/2023.04.20/Practice 1/NightGreetingImpl.java
@@ -0,0 +1,7 @@
+package exam1;
+public class NightGreetingImpl implements Greeting {
+    @Override
+    public void greet() {
+        System.out.println("안녕히 주무세요.");
+    }
+}
