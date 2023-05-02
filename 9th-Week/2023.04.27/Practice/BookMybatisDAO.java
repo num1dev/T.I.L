14306bdf0918f1c07dc14fb96b521ad0559a4128
Lucy <122321793+num1dev@users.noreply.github.com>
2023-05-02 11:01:16 +0900

    Upload : Practice - Spring Framework Basic 07

---
+++ 9th-Week/2023.04.27/Practice/BookMybatisDAO.java
@@ -0,0 +1,90 @@
+package com.example.springedu.dao;
+
+import com.example.springedu.domain.*;
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class BookMybatisDAO {
+	@Autowired
+	SqlSession session;	
+	public List<BookDTO> selectE1() {
+		List<BookDTO> list = null;
+		try {
+			String statement = "lab1.exam1";
+			list = session.selectList(statement);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+	public List<BookDTO> searchE2(){
+		List<BookDTO> list = null;
+		try {
+			String statement = "lab1.exam2";
+			list = session.selectList(statement);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+
+	public List<BookDTO> searchE3(){
+		List<BookDTO> list = null;
+		try {
+			String statement = "lab1.exam3";
+			list = session.selectList(statement);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+
+	public List<BookDTO> searchE4(){
+		List<BookDTO> list = null;
+		try {
+			String statement = "lab1.exam4";
+			list = session.selectList(statement);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return list;
+
+	}
+
+	public List<BookDTO> searchE5(){
+		List<BookDTO> list = null;
+		try {
+			String statement = "lab1.exam5";
+			list = session.selectList(statement);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+
+	public List<BookDTO> searchE6(String title){
+		List<BookDTO> list = null;
+		try {
+			String statement = "lab1.exam6";
+			list = session.selectList(statement, title);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+	public List<BookDTO> selectE7(){
+		List<BookDTO> list = null;
+		try {
+			String statement = "lab1.exam7";
+			list = session.selectList(statement);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+
+}
